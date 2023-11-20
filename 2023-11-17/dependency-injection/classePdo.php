31914b6ee32c6ccdf15b903c1f8259ef0a89f0a7
giorgiomodeo-admin <giorgio.modeo@gmail.com>
2023-11-20 14:17:51 +0100

    skeleton framework

--- 2023-11-17/dependency-injection/classePdo.php
+++ 2023-11-17/dependency-injection/classePdo.php
@@ -1,5 +1,26 @@
 <?php
 class classePdo
 {
+    // simple class use pdo
+    private $pdo;
+    public function __construct()
+    {
+        $this->pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+    }
+
+    public function getAllArticles()
+    {
+        $req = $this->pdo->prepare('SELECT * FROM articles');
+        $req->execute();
+        $articles = $req->fetchAll(PDO::FETCH_OBJ);
+        return $articles;
+    }
     
+    public function getArticle($id)
+    {
+        $req = $this->pdo->prepare('SELECT * FROM articles WHERE id = ?');
+        $req->execute([$id]);
+        $article = $req->fetch(PDO::FETCH_OBJ);
+        return $article;
+    }
 }
